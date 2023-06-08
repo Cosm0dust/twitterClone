2cbde3e497b515399777f580ac34bbae2e44a7cb
Cosm0dust <vanyakotelevskiy@gmail.com>
2023-06-08 21:26:26 +0300

    finish

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -4,12 +4,12 @@ export const authOptions = {
     // Configure one or more authentication providers
     providers: [
         GoogleProvider({
-            clientId: "595492875956-v5jj65onffmhhqgpubf418to0j9812ae.apps.googleusercontent.com",
-            clientSecret: "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",
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_SECRET,
         }),
         // ...add more providers here
     ],
-    secret: "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImpvaG4uZG9lIn0.EFzxw9w1nZys3WQgOwqQfSwdCzcjxkTKwZNL5Z5lWyP_aU2a3wboZFX3spT5R2R8yVGKKDJ0-wC7YGhYSwcrKQ",
+    secret: process.env.JWT_SECRET,
     pages:{
         signIn: "/login"
     }
